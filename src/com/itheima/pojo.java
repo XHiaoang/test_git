257dd1b73ea966d3d3f7818da12670b74e01da17
xiao <205501564@qq.com>
2019-01-24 23:26:13 +0800

    tijiao

---
+++ src/com/itheima/pojo.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class pojo {
+
+    public static void main(String[] args) {
+        System.out.println("sdadasdsadasdasds");
+        System.out.println("sdadasdsadasdasds");
+        System.out.println("sdadasdsadasdasds");
+        System.out.println("sdadasdsadasdasds");
+        System.out.println("sdadasdsadasdasds");
+    }
+}
